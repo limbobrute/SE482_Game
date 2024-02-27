0e95554ae322ee57c56b47a46013df6838b5a2cd
LimboBrute <wihprahl@gmail.com>
2024-02-26 20:46:15 -0600

    Updated World Gen
    Changed world gen from Wave function collapse to Perlin noise.

---
+++ Assets/Scripts/TechTreeNode.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TechTreeNode", menuName = "ScriptableObjects/TechTreeNode", order = 1)]
+public class TechTreeNode : ScriptableObject
+{
+    
+    
+}
