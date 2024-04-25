4c90518f500a87df73dae8fea7b9f76e25716910
LimboBrute <wihprahl@gmail.com>
2024-04-25 17:14:06 -0500

    Additional models added to tiles
    Set up method to have tiles place models on themselves to give them some uniqueness from each other.

--- Assets/Scripts/Map/Tile.cs
+++ Assets/Scripts/Map/Tile.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     private MapGen map;
+    private List<GameObject> madeModels = new List<GameObject>();
     public string tileType;
     public List<BuildingDataNew> possibleBuildings;
+    public GameObject ModelPlacer;
+    public GameObject[] models;
+    public int NumberOfModels = 3;
 
-    [Serializable]
+    /*[Serializable]
     public struct EmptyNeigbours
     {
         public GameObject Up;
@@ -20,7 +24,25 @@ public class Tile : MonoBehaviour
         public GameObject BottomRight;
 
     }
-    public EmptyNeigbours Eneigbhours;
+    public EmptyNeigbours Eneigbhours;*/
+    [Serializable] public struct XBand
+    {
+        public float UpperX; 
+        public float LowerX;
+    }
+    [Serializable] public struct YBand
+    {
+        public float UpperY;
+        public float LowerY;
+    }
+    [Serializable] public struct ZBand
+    {
+        public float UpperZ;
+        public float LowerZ;
+    }
+    [Tooltip("Range for a model's x transform.position value")]public XBand xband;
+    [Tooltip("Range for a model's y transform.position value")] public YBand yband;
+    [Tooltip("Range for a model's z transform.position value")] public ZBand zband;
     [Tooltip("Upper Z scale value that this tile can be found at.")]
     public float UpperRange = 0f;
     [Tooltip("Lower Z scale value that this tile can be found at.")]
@@ -31,6 +53,37 @@ public class Tile : MonoBehaviour
     private void Awake()
     {
         map = MapGen.instance;
+    }
+
+    private void Start()
+    {
+        
+        if (NumberOfModels > 0)
+        {
+            for (int i = 0; i < NumberOfModels; i++)
+            {
+                int rand = 0;
+                if (models.Length > 1)
+                { rand = UnityEngine.Random.Range(0, models.Length - 1); }
+                GameObject model = Instantiate(models[rand], transform.position = ModelPlacer.transform.position, Quaternion.identity);
+                model.transform.parent = this.gameObject.transform;
+                Vector3 localLocation = new Vector3(UnityEngine.Random.Range(xband.LowerX, xband.UpperX), UnityEngine.Random.Range(yband.LowerY, yband.UpperY), UnityEngine.Random.Range(zband.LowerZ, zband.UpperZ));
+                model.transform.localPosition = localLocation;
+                float xRotate = UnityEngine.Random.Range(0f, 360f);
+                //model.transform.localRotation = Quaternion.Euler(xRotate, model.transform.localRotation.y, model.transform.localRotation.z);
+                //model.transform.parent = null;
+                //model.transform.localScale = new Vector3(0.01f, 0.1f, (this.gameObject.transform.localScale.z/1000));
+                madeModels.Add(model);
+            }
+        }
+        transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+
+        foreach(GameObject model in madeModels)
+        { 
+            model.transform.parent = null;
+            float yRotate = UnityEngine.Random.Range(0f, 360f);
+            model.transform.localRotation = Quaternion.Euler(model.transform.localRotation.x, yRotate, model.transform.localRotation.z);
+        }
     }
 
     /*public void UpdateNeigbhours()
